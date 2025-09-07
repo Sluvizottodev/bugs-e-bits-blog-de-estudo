6b3a92c7a101207ffe9b034e020f309c993a8577
SLuvizotto <luvizottodev@gmail.com>
2025-09-06 23:26:37 -0300

    feat: adiciona arquivo posts.js com dados de exemplo

---
+++ data/posts.js
@@ -0,0 +1,17 @@
+export const posts = [
+    {
+        id: 1,
+        titulo: "Exemplo de Post",
+        resumo: "Resumo. Clique para ler mais."
+    },
+    {
+        id: 2,
+        titulo: "Segundo Post",
+        resumo: "Resumo. Clique para ler mais."
+    },
+    {
+        id: 3,
+        titulo: "Terceiro Post",
+        resumo: "Resumo. Clique para ler mais."
+    }
+];
