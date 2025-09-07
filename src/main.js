6465f2418f539b39dfb51af46898cb13454f3411
SLuvizotto <luvizottodev@gmail.com>
2025-09-06 23:25:23 -0300

    feat: adiciona chamada para renderizar posts no arquivo main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import loadHeader from './utils/loadHeader.js';
+import { posts } from './data/posts';
+import { renderPosts } from './components/renderPosts.js';
 
 loadHeader();
+
+renderPosts(posts, 'posts');
