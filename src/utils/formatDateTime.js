b3d0e1c685b31c60c5239f30376b1b1e3ab4469a
SLuvizotto <luvizottodev@gmail.com>
2025-09-12 00:45:52 -0300

    feat: adiciona função formatDateTime para formatação de datas

---
+++ src/utils/formatDateTime.js
@@ -0,0 +1,10 @@
+function formatDateTime(date) {
+    const dataObj = new Date(date)
+
+    const options = {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'};
+    return dataObj.toLocaleString('pt-BR', options)
+}
+
+//teste
+console.log(formatDateTime('2025-09-12T15:45:30'))
+console.log(formatDateTime('banana'))
