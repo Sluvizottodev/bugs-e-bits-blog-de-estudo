950f4d1114f168690aa8edc1691aa377a9d2e7d8
SLuvizotto <luvizottodev@gmail.com>
2025-09-14 19:52:44 -0300

    fix: garante carregamento correto do rodapé

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import loadHeader from './utils/loadHeader.js';
+import loadFooter from './utils/loadFooter.js';
 import { posts } from './data/posts.js'; 
 import { renderPosts } from './utils/renderPosts.js';
+import loadFooter from './utils/loadFooter.js';
 
 loadHeader();
+loadFooter();
 
 renderPosts(posts, 'featured-posts');
 renderPosts(posts, 'posts');
