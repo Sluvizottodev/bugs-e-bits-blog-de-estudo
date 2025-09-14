9338b93fd59e539ff123125a8c9c2093dc6a0be0
SLuvizotto <luvizottodev@gmail.com>
2025-09-14 19:15:36 -0300

    refactor: corrige caminhos de importação e organiza chamadas de renderização de posts

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import loadHeader from './utils/loadHeader.js';
-import { posts } from './data/posts';
-import { renderPosts } from './components/renderPosts.js';
+import { posts } from './data/posts.js'; 
+import { renderPosts } from './utils/renderPosts.js';
 
 loadHeader();
 
+renderPosts(posts, 'featured-posts');
 renderPosts(posts, 'posts');
